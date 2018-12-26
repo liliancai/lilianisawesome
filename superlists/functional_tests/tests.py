667bdc52efd814aa5f044e2f830b2584c595e6f5
yushun <yushun>
2018-12-26 15:23:25 +0100

    LiveServerTestCase

---
+++ superlists/functional_tests/tests.py
@@ -0,0 +1,71 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+#import unittest
+import time
+#from selenium.webdriver.support.ui import WebDriverWait
+
+class NewVisitorTest(LiveServerTestCase): 
+	def setUp(self): 
+		self.browser = webdriver.Firefox() 
+		self.browser.implicitly_wait(30)
+	def tearDown(self): 
+	    self.browser.quit()
+
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table') 
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# Edith has heard about a cool new online to-do app. She goes 
+		# to check out its homepage 
+		self.browser.get(self.live_server_url)
+
+		# She notices the page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title)
+
+		header_text = self.browser.find_element_by_tag_name( 'h1' ).text
+
+		self.assertIn('To-Do', header_text)
+	 	
+		# She is invited to enter a to-do item straight away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+
+		# She types "Buy peacock feathers" into a text box 
+
+		inputbox.send_keys('Buy peacock feathers')
+		# When she hits enter, the page updates, and now the page inputboxTabError: inconsistent use of tabs and spaces in indentation
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		inputbox = self.browser.find_element_by_id('id_new_item') 
+		inputbox.send_keys('Use peacock feathers to make a fly') 
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+
+		# "1: Buy peacock feathers" as an item in a to-do list
+		#self.assertIn('1: Buy peacock feathers') 
+		#self.assertIn('2: Use peacock feathers to make a fly')
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
+
+
+		# There is still a text box inviting her to add another item. She
+		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+		# The page updates again, and now shows both items on her list
+		# Edith wonders whether the site will remember her list. Then she sees
+		# that the site has generated a unique URL for her -- there is some
+		# explanatory text to that effect.
+		# She visits that URL - her to-do list is still there. # Satisfied, she goes back to sleep
+		#A REAMINDER
+		self.fail('Finish the test!')
+
+
+#for terminal 
+#if __name__ == '__main__': 
+	#unittest.main(warnings='ignore') 
