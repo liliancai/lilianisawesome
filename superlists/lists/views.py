8fe9c049ea7bf531290ec1892ae7b519b522ed3f
yushun <yushun>
2018-12-27 23:53:12 +0100

    new model list

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -8,7 +8,8 @@ def home_page(request):
 	return render(request, 'home.html')
 '''
 from django.shortcuts import render,redirect
-from lists.models import Item
+from lists.models import Item,List
+
 '''
 def home_page(request):
 	item = Item()
@@ -29,11 +30,13 @@ def home_page(request):
 	})
 '''
 def home_page(request):
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text']) 
-		return redirect('/lists/the-only-list-in-the-world/')
 	return render(request, 'home.html')
 
 def view_list(request):
 	items = Item.objects.all()
 	return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+	list_ = List.objects.create() 
+	Item.objects.create(text=request.POST['item_text'], list=list_)
+	return redirect('/lists/the-only-list-in-the-world/')
