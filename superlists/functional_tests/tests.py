d5fe7a60763da0d6d5445de81db9e4a1bff3b509
yushun <yushun>
2018-12-31 10:42:54 +0100

    layout+styling and fixed a bug in home.hmtl!

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 #import unittest
 import time
+from lists.models import Item, List
+
 #from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import WebDriverException
 MAX_WAIT = 10	
@@ -13,7 +15,7 @@ class NewVisitorTest(LiveServerTestCase):
 		self.browser.implicitly_wait(30)
 	def tearDown(self): 
 	    self.browser.quit()
-
+	
 	def wait_for_row_in_list_table(self,row_text):
 		start_time=time.time()
 		while True:
@@ -26,7 +28,7 @@ class NewVisitorTest(LiveServerTestCase):
 				if time.time() - start_time > MAX_WAIT:
 					raise e
 				time.sleep(0.5)
-
+	
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table') 
 		rows = table.find_elements_by_tag_name('tr')
@@ -45,6 +47,7 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn('To-Do', header_text)
 	 	
 		# She is invited to enter a to-do item straight away
+		#it's noti working!
 		inputbox = self.browser.find_element_by_id('id_new_item')
 
 		# She types "Buy peacock feathers" into a text box 
@@ -53,6 +56,10 @@ class NewVisitorTest(LiveServerTestCase):
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
+		#debug,check if send keys works or not
+		#saved_items = Item.objects.all()
+		#self.assertEqual(saved_items.count(), 2)
+		#doesn't work
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
 		# There is still a text box inviting her to add another item. She
@@ -60,10 +67,6 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item') 
 		inputbox.send_keys('Use peacock feathers to make a fly') 
 		inputbox.send_keys(Keys.ENTER)
-		#time.sleep(1)
-
-
-
 		
 		# The page updates again, and now shows both items on her list
 		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
@@ -90,7 +93,8 @@ class NewVisitorTest(LiveServerTestCase):
 		edith_list_url = self.browser.current_url
 		self.assertRegex(edith_list_url, '/lists/.+')
 		#Now a new user, Francis, comes along to the site.
-		## We use a new browser session to make sure that no information ## of Edith's is coming through from cookies etc self.browser.quit()
+		## We use a new browser session to make sure that no information 
+		## of Edith's is coming through from cookies etc self.browser.quit()
 		self.browser = webdriver.Firefox()
 		# Francis visits the home page. There is no sign of Edith's # list
 		self.browser.get(self.live_server_url)
@@ -112,8 +116,28 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
 		# Satisfied, they both go back to sleep
-
-
-#for terminal 
-#if __name__ == '__main__': 
-	#unittest.main(warnings='ignore') 
+	
+	def test_layout_and_styling(self):
+		# Edith goes to the home page 
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+		# She notices the input box is nicely centered
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+		)
+		# She starts a new list and sees the input is nicely
+		# centered there too
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+		self.wait_for_row_in_list_table('1: testing')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+		)
+	
