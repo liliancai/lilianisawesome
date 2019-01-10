a2930ff3ab86a77d376fb21a4f16ad65e75d8c97
yushun <yushun>
2019-01-10 13:11:17 +0100

    mv test.py base.py and write me

--- source/functional_tests/test_simple_list_creation.py
+++ source/functional_tests/test_simple_list_creation.py
@@ -1,41 +1,11 @@
-#from django.test import LiveServerTestCase
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-#import unittest
-import time
-from lists.models import Item, List
+from .base import FunctionalTest
+from unittest import skip
 
-#from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import WebDriverException
-MAX_WAIT = 10	
-
-class NewVisitorTest(StaticLiveServerTestCase): 
-	def setUp(self): 
-		self.browser = webdriver.Firefox() 
-		self.browser.implicitly_wait(30)
-	def tearDown(self): 
-	    self.browser.quit()
-	'''
-	def wait_for_row_in_list_table(self,row_text):
-		start_time=time.time()
-		while True:
-			try:
-				table=self.browser.find_element_by_id('id_list_table')
-				rows=table.find_elements_by_tag_name('tr')
-				self.assertIn(row_text,[row.text for row in rows])
-				return
-			except (AssertionError, WebDriverException) as e:
-				if time.time() - start_time > MAX_WAIT:
-					raise e
-				time.sleep(0.5)
-	
-	def check_for_row_in_list_table(self, row_text):
-		table = self.browser.find_element_by_id('id_list_table') 
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text, [row.text for row in rows])
-	
-	def test_can_start_a_list_and_retrieve_it_later(self):
+class NewVisitorTest(FunctionalTest): 
+	@skip
+	def test_can_start_a_list_for_one_user(self):
 		# Edith has heard about a cool new online to-do app. She goes 
 		# to check out its homepage 
 		self.browser.get(self.live_server_url)
@@ -48,8 +18,11 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.assertIn('To-Do', header_text)
 	 	
 		# She is invited to enter a to-do item straight away
-		#it's noti working!
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.get_item_input_box()
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
 
 		# She types "Buy peacock feathers" into a text box 
 		inputbox.send_keys('Buy peacock feathers')
@@ -57,32 +30,18 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
-		#debug,check if send keys works or not
-		#saved_items = Item.objects.all()
-		#self.assertEqual(saved_items.count(), 2)
-		#doesn't work
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
-		inputbox = self.browser.find_element_by_id('id_new_item') 
-		inputbox.send_keys('Use peacock feathers to make a fly') 
-		inputbox.send_keys(Keys.ENTER)
-		
+		self.add_list_item('Use peacock feathers to make a fly')
 		# The page updates again, and now shows both items on her list
 		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-
-		
-		# Edith wonders whether the site will remember her list. Then she sees
-		# that the site has generated a unique URL for her -- there is some
-		# explanatory text to that effect.
-		# She visits that URL - her to-do list is still there. 
 		# Satisfied, she goes back to sleep
-		#A REAMINDER
-		#self.fail('Finish the test!')
-
+	
+	#not sure refactored or not, leave it as find_element_by_id instead of self.add_list_item()
 	def test_multiple_users_can_start_lists_at_different_urls(self): 
 		# Edith starts a new to-do list 
 		self.browser.get(self.live_server_url)
@@ -94,53 +53,32 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		edith_list_url = self.browser.current_url
 		self.assertRegex(edith_list_url, '/lists/.+')
 		#Now a new user, Francis, comes along to the site.
+
 		## We use a new browser session to make sure that no information 
 		## of Edith's is coming through from cookies etc self.browser.quit()
 		self.browser = webdriver.Firefox()
+
 		# Francis visits the home page. There is no sign of Edith's # list
 		self.browser.get(self.live_server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
+
 		# Francis starts a new list by entering a new item. He
 		# is less interesting than Edith...
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Buy milk')
 		inputbox.send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy milk')
+
 		# Francis gets his own unique URL
 		francis_list_url = self.browser.current_url
 		self.assertRegex(francis_list_url, '/lists/.+')
 		self.assertNotEqual(francis_list_url, edith_list_url)
+
 		# Again, there is no trace of Edith's list
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
 		# Satisfied, they both go back to sleep
-	'''
-	def test_layout_and_styling(self):
-		# Edith goes to the home page 
-		self.browser.get(self.live_server_url)
-		self.browser.set_window_size(1024, 768)
-		
-		# She notices the input box is nicely centered
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.size['width'] / 2,
-			512,
-			delta=10
-		)
-		
-		# She starts a new list and sees the input is nicely
-		# centered there too
-		inputbox.send_keys('testing')
-		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-		self.wait_for_row_in_list_table('1: testing')
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.size['width'] / 2,
-			512,
-			delta=10
-		)
 	
