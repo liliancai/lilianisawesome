4d5defcdc054786e09a9b6f2df16d20d9355f9f1
yushun <yushun>
2019-01-20 23:02:32 +0100

    form duplication validation

--- source/lists/models.py
+++ source/lists/models.py
@@ -8,4 +8,5 @@ class List(models.Model):
 class Item(models.Model):
 	text = models.TextField(default='')
 	list = models.ForeignKey(List,on_delete=models.CASCADE)
-
+	class Meta:
+		unique_together=('text','list')
