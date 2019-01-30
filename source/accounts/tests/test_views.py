def631188b33ea70f53d83b782de69a5021c1d73
yushun <yushun>
2019-01-30 11:33:02 +0100

    use patch to mock sendmail function

--- source/accounts/tests/test_views.py
+++ source/accounts/tests/test_views.py
@@ -1,4 +1,5 @@
 #test after sending email if the webpage redirect to homepage
+from unittest.mock import patch
 from django.test import TestCase
 from unittest import skip
 import accounts.views
@@ -18,20 +19,14 @@ class SendLoginEmailViewTest(TestCase):
 			})
 		self.assertRedirects(response, '/')
 
-	def test_sends_mail_address_from_post(self):
+
+	@patch('accounts.views.send_mail')	
+	def test_sends_mail_address_from_post(self,mock_send_mail):
 		#you wouldn't wanna use the real email-send function
 		#so we need write a mock email-send function same with the one in view
 		#to test without mess the real email address
 
-		self.send_mail_called=False
-
-		#like a reciever recieing all the attributes from send_email()
-		#then use assertEqual to check if the send_email() pass all the right content
-		#okay, but what's the point?????
-		#cus if use email.to, have to accually use emailbox as reciever
-		#which will produce tons of junk email
-		#so fake send mail function accually send those mail content to testcase itself instead of calling send_email to send real mail box
-		#mocking here is acctually replace the email send api~
+		'''
 		def fake_send_mail(subject,body,from_email,to_list):
 			self.send_mail_called=True
 			self.subject=subject
@@ -39,20 +34,14 @@ class SendLoginEmailViewTest(TestCase):
 			self.from_email=from_email
 			#I wanna chang this to_list to to_email!
 			self.to_list=to_list
+		'''
 
-
-		accounts.views.send_mail=fake_send_mail
-
-		#then call the link!
-		#but one question here,how can send_login_email call views.send_email,urls???
-		#okay,I get now, cus of 'import accounts.views!', so the namespacing contains accounts.views.send_email
 		self.client.post('/accounts/send_login_email',data={
 			'email':'test@mockto.com'
 		})
 		#then check the email content is what supposed to or not
-
-		self.assertTrue(self.send_mail_called)
-		self.subject='Your login link from todolist'
-		#self.body=
-		self.from_email='test@mockfrom.com'
-		self.to_list='test@mockto.com'
+		self.assertTrue(mock_send_mail.send_mail_called)
+		(subject,body,from_email,to_email),kwargs=mock_send_mail.call_args
+		self.assertEqual(subject,'Your log in link from todolist')
+		self.assertEqual(from_email,'test@mockfrom.com')
+		self.assertEqual(to_email,['test@mockto.com'])
