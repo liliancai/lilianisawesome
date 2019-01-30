1b33d86999a9964709ff3a071c67cd435622e4ce
yushun <yushun>
2019-01-30 21:17:54 +0100

    test if comments in test_views.py remoted or not

--- source/accounts/tests/test_views.py
+++ source/accounts/tests/test_views.py
@@ -5,14 +5,7 @@ from unittest import skip
 import accounts.views
 
 class SendLoginEmailViewTest(TestCase):
-	#AssertionError: 301 != 302 : Response didn't redirect as expected: Response code was 301 (expected 302)
-	#page 324, test later
-	#I know what's wrong, is url again!!!!!!!!!!!
-	#okay, in the urls.py I written the url(r'^send_login_email/$')
-	#after remove the '/' this test passed and as fake_send_mail got called, 
-	#okay, always double check syntax error, and triple check url!
-	#peace out~
-	#@skip
+
 	def test_redirects_to_home_page(self):
 		response=self.client.post('/accounts/send_login_email',data={
 			'email':'test@test.com'
@@ -22,9 +15,6 @@ class SendLoginEmailViewTest(TestCase):
 
 	@patch('accounts.views.send_mail')	
 	def test_sends_mail_address_from_post(self,mock_send_mail):
-		#you wouldn't wanna use the real email-send function
-		#so we need write a mock email-send function same with the one in view
-		#to test without mess the real email address
 
 		'''
 		def fake_send_mail(subject,body,from_email,to_list):
